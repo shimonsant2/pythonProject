dac36798fc22ffb79186e221d159ae05e878a53d
Shimon Sant <122385952+shimonsant2@users.noreply.github.com>
2023-01-12 18:54:28 +0200

    Close the file after using it

--- targil1.py
+++ targil1.py
@@ -221,6 +221,7 @@ if __name__ == '__main__':
     f.close()
     f = open("demofile2.txt", "r")
     print(f.read())
+    f.close()
 
     f = open("demofile3.txt", "w")
     f.write("Now the file has more content!!!!!!!!")
