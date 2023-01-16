bdc23f12bee0304595091581b338c5b7e6e7b847
Shimon Sant <122385952+shimonsant2@users.noreply.github.com>
2023-01-16 17:57:50 +0200

    Fix the test_example.py tests

--- test_example.py
+++ test_example.py
@@ -4,8 +4,12 @@ def func(x):
 
 
 def test_answer1():
-    assert func(3) == 5
+    assert func(4) == 5
 
 
 def test_answer2():
+    assert func(3) == 4
+
+
+def test_answer3():
     assert func(3) == 4
